6cac87caaca61691db716794751ef387f41fae49
HAL919 <60395718+hareta0109@users.noreply.github.com>
2022-08-27 00:11:26 +0900

    Add files via upload

---
+++ submissions/abc265/b.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MOD (long long int)(998244353)
+#define ll long long int
+#define rep(i,n) for(int i=0; i<(int)(n); i++)
+#define reps(i,n) for(int i=1; i<=(int)(n); i++)
+#define REP(i,n) for(int i=n-1; i>=0; i--)
+#define REPS(i,n) for(int i=n; i>0; i--)
+#define FOR(i,a,b) for(int i=a; i<(int)(b); i++)
+#define ALL(x) (x).begin(),(x).end()
+#define RALL(a) (a).rbegin(), (a).rend()
+#define CLR(a) memset((a), 0 ,sizeof(a))
+#define PB push_back
+#define MP make_pair
+#define SP << " " <<
+const int INF = 1001001001;
+const ll LINF = 100100100100100100;
+const double EPS = 1e-10;
+const long double PI  = acos(-1.0L);
+typedef pair<int,int> PII;
+typedef pair<ll,ll> PLL;
+typedef pair<double,double> PDD;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<ll> VL;
+typedef vector<VL> VVL;
+#define chmax(a,b) a = (((a)<(b))?(b):(a))
+#define chmin(a,b) a = (((a)>(b))?(b):(a))
+
+__attribute__((constructor))
+void initial(){
+	cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+	cout << fixed << setprecision(15);
+}
+
+ll lpow(ll a, ll b){
+  if(b==0) return 1;
+  else if(b%2==0){ll memo = lpow(a,b/2); return memo*memo;}
+  else return lpow(a,b-1)*a;
+}
+
+signed main() {
+	int n,m; ll t; cin>>n>>m>>t;
+	VL a(n-1); VI x(m),y(m);
+	rep(i,n-1) cin>>a[i]; rep(i,m) cin>>x[i]>>y[i];
+
+	bool able = false;
+	int b = 0;
+	rep(i,n-1){
+		if(b<m){
+			if(x[b]==i+1) t+=y[b], b++;
+		}
+		
+		t-=a[i];
+		if(t<=0) break;
+		if(i==n-2) able = true;
+	}
+	cout << (able?"Yes":"No") << endl;
+	return 0;
+}
